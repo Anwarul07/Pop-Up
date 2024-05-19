124083402dcce72f27a180a8b3472f505574a09d
Anwarul07 <mdanwarul064@gmail.com>
2024-05-20 00:17:18 +0530

    update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const popup= document.getElementById("popup");
+
+function openpopup(params) {
+    popup.classList.add("openpop");
+}
+function closepopup(params) {
+    popup.classList.remove("openpop");
+}
